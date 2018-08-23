4db4ba0e630478c55747a874a8121acbf91e6cc7
aamir77i <aamir.irshad@cern.ch>
2018-08-23 09:35:52 +0200

    gui added

---
+++ lampswitch.m
@@ -0,0 +1,27 @@
+%%
+function lampswitch
+fig = uifigure('Position',[100 100 350 275]);
+
+% Create lamp
+lmp = uilamp(fig,...
+    'Position',[165 75 20 20],...
+    'Color','green');
+
+% Create switch
+uiswitch(fig,'toggle',...
+    'Items',{'Go','Stop'},...    
+    'Position',[165 160 20 45],...
+    'ValueChangedFcn',@switchMoved); 
+
+% ValueChangedFcn callback
+function switchMoved(src,event)  
+    switch src.Value
+        case 'Go'
+            lmp.Color = 'green';
+            find_dead_channels();
+            
+        case 'Stop'
+            lmp.Color = 'red';
+   end
+end
+end
